a921b51b91a47f2768cbe80ebec3f59bf17c916e
rkoyae <rkoyae@gmail.com>
2022-02-27 00:26:41 +0900

    make form

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,1 +1,16 @@
+from cProfile import label
+from tkinter import Widget
+from unicodedata import name
+from django import forms
 
+class HelloForm(forms.Form):
+    username = forms.CharField(label='username',\
+        widget=forms.TextInput(attrs={'class':'form-control'}))
+    emailaddress = forms.CharField(label='emailaddress',\
+        widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(label='password',\
+        widget=forms.TextInput(attrs={'class':'form-control'}))
+    passwordconfirmation = forms.CharField(label='passwordconfirmation',\
+        widget=forms.TextInput(attrs={'class':'form-control'}))
+    img = forms.IntegerField(label='img',\
+        widget=forms.NumberInput(attrs={'class':'form-control'}))
--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,26 @@
+from unicodedata import name
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from .forms import HelloForm
 
 
 def index(request):
     return render(request, "myapp/index.html")
 
 def signup_view(request):
-    return render(request, "myapp/signup.html")
+    params = {
+        'title':'hello',
+        'message':'',
+        'form': HelloForm()
+    }
+    if (request.method == 'POST'):
+        params['message'] = 'Username: ' + request.POST['username']+ \
+            '<br>Emailaddress: ' + request.POST['emailaddress'] + \
+            '<br>Password: ' + request.POST['password'] + \
+            '<br>Password Confirmation: ' +request.POST['passwordconfirmation'] + \
+            '<br>img: ' + request.POST['img']          
+        params['form'] = HelloForm(request.POST)
+    return render(request, 'myapp/signup.html', params) 
 
 def login_view(request):
     return render(request, "myapp/login.html")
@@ -19,3 +33,5 @@ def talk_room(request):
 
 def setting(request):
     return render(request, "myapp/setting.html")
+
+
