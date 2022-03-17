a1770d5780f3ddd916ffa93abc20dd31e28d84a3
Yutatsu <yutatsu7@gmail.com>
2022-03-17 19:28:25 +0900

    commit

--- intern/settings.py
+++ intern/settings.py
@@ -139,7 +139,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = BASE_DIR/"staticfiles"
+STATIC_URL = "/static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
