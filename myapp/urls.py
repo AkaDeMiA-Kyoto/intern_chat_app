0cea756b4c7e90ded3a8cad71fb744fe335c28d9
navyracooon <navyracooon@gmail.com>
2022-04-26 19:29:07 +0900

    フロント完成

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,4 +8,12 @@ urlpatterns = [
     path('friends', views.friends, name='friends'),
     path('talk_room', views.talk_room, name='talk_room'),
     path('setting', views.setting, name='setting'),
+    path('setting_username', views.setting_username, name='setting_username'),
+    path('setting_mailaddress', views.setting_mailaddress, name='setting_mailaddress'),
+    path('setting_icon', views.setting_icon, name='setting_icon'),
+    path('setting_password', views.setting_password, name='setting_password'),
+    path('setting_username_completed', views.setting_username_completed, name='setting_username_completed'),
+    path('setting_mailaddress_completed', views.setting_mailaddress_completed, name='setting_mailaddress_completed'),
+    path('setting_icon_completed', views.setting_icon_completed, name='setting_icon_completed'),
+    path('setting_password_completed', views.setting_password_completed, name='setting_password_completed'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -18,3 +18,27 @@ def talk_room(request):
 
 def setting(request):
     return render(request, "myapp/setting.html")
+
+def setting_username(request):
+    return render(request, "myapp/setting_username.html")
+
+def setting_mailaddress(request):
+    return render(request, "myapp/setting_mailaddress.html")
+
+def setting_icon(request):
+    return render(request, "myapp/setting_icon.html")
+
+def setting_password(request):
+    return render(request, "myapp/setting_password.html")
+
+def setting_username_completed(request):
+    return render(request, "myapp/setting_username_completed.html")
+
+def setting_mailaddress_completed(request):
+    return render(request, "myapp/setting_mailaddress_completed.html")
+
+def setting_icon_completed(request):
+    return render(request, "myapp/setting_icon_completed.html")
+
+def setting_password_completed(request):
+    return render(request, "myapp/setting_password_completed.html")
