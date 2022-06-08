9c4d5019c5ed625bfb93470a402c63c18003bfb2
tsuji51 <qingtaishi@gmail.com>
2022-06-08 16:01:53 +0900

    index追加

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,1 +1,11 @@
+from   django import froms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class signup_form (UserCreationForm):
+    class meta:
+         model = User
+         fields = ("username", "email", "icon")
 
