1a5743b308e3139dcbe7c9e5284344d6909e2e1e
kanyamo <kanajir@yahoo.co.jp>
2022-06-10 15:34:15 +0900

    デプロイ用6

--- intern/urls.py
+++ intern/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -22,8 +22,8 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
-    url(r'^media/(?P<path>.*)$', serve,
-        {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve,
+            {'document_root': settings.MEDIA_ROOT}),
 ]
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
