4e49831a4fa23a35269de5dad0f4c3aa1a9e5713
tsuji51 <qingtaishi@gmail.com>
2022-06-15 20:53:23 +0900

    signup更新

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,11 +1,16 @@
-from   django import froms
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class signup_form (UserCreationForm):
-    class meta:
+class SignUpForm (UserCreationForm):
+    email = forms.EmailField(
+        max_length=254,
+        help_text='必須 有効なメールアドレスを入力してください。',
+        label='Eメールアドレス'
+    )
+    class Meta:
          model = User
-         fields = ("username", "email", "icon")
+         fields = ('email', 'username', 'icon'  )
 
