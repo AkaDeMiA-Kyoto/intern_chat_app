f7f48ae60f83bd1b3da12aba6b9712e6a534ff82
Hajime-Kujo <waspsishf@gmail.com>
2022-06-22 18:43:23 +0900

    add localsettings

--- intern/settings.py
+++ intern/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -130,3 +132,7 @@ LOGOUT_REDIRECT_URL=''
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
+
+try:
+    from .local_settings import *
+except ImportError:pass
