dd107405b61172aae90c320e195696d1751bbc19
Hatimitukunsan <marusu200391@gmail.com>
2022-11-01 20:37:26 +0900

    create first model

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    nama = models.CharField(max_length = 200)
+    email = models.CharField(max_length = 200)
+    password = models.CharField(max_length = 200)
+
+
+class Friend(models.Model):
+    name = models.CharField(max_length = 200)
+
+class Talk(models.Model):
+    user_name = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    chat_partner_name = models.CharField(max_length = 200)
+    message = models.CharField(max_length = 1000)
+    date = models.DateTimeField()
