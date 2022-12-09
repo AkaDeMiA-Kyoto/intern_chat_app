d05f2ac2d9acc554a4e4a330cd1a5e9c6e8e7c15
Hatimitukunsan <marusu200391@gmail.com>
2022-12-09 16:44:45 +0900

    commit

--- myapp/models.py
+++ myapp/models.py
@@ -3,16 +3,15 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    nama = models.CharField(max_length = 200)
-    email = models.CharField(max_length = 200)
-    password = models.CharField(max_length = 200)
+    # nama = models.CharField(max_length = 200)
+    # email = models.CharField(max_length = 200)
+    # password = models.CharField(max_length = 200)
 
 
-class Friend(models.Model):
-    name = models.CharField(max_length = 200)
+# class Friend(models.Model):
+#     name = models.CharField(max_length = 200)
 
-class Talk(models.Model):
-    user_name = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    chat_partner_name = models.CharField(max_length = 200)
-    message = models.CharField(max_length = 1000)
-    date = models.DateTimeField()
+# class Talk(models.Model):
+#     user_name = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+#     chat_partner_name = models.CharField(max_length = 200)
+#     message = models.CharField(max_length = 1000)
