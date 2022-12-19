2635c777113f9c4e203f5d683b243326ed10a9ce
hkansei <onyaka123456789@gmail.com>
2022-12-19 17:15:49 +0900

    long time no see

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,1 +1,28 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth import forms as authform
+
+from myapp.models import MyUser
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = MyUser
+        fields = ('username', 'email', 'img')
+
+    def __init__(self, *args, **kw):
+        super().__init__(*args, **kw)
+        self.fields['username'].label = 'ユーザー名'
+        self.fields['username'].widget.attrs['placeholder'] = "新規ユーザー名..."
+        self.fields['email'].label = 'メールアドレス'
+        self.fields['email'].widget.attrs['placeholder'] = "aaa@example.com"
+        self.fields['password1'].label = 'パスワード'
+        self.fields['password1'].widget.attrs['placeholder'] = "新規パスワード..."
+        self.fields['password2'].label = 'パスワード（確認）'
+        self.fields['password2'].widget.attrs['placeholder'] = "再度入力してください..."
+
+class LoginForm(authform.AuthenticationForm):
+    def __init__(self, *args, **kw):
+        super().__init__(*args, **kw)
+        for field in self.fields.values():
+            field.widget.attrs['placeholder'] = field.label
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,20 +1,62 @@
 from django.shortcuts import redirect, render
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.views import LoginView
+from . import forms
+from .models import MyUser, ChatContent
+from django.db.models import Q
 
 def index(request):
     return render(request, "myapp/index.html")
 
 def signup_view(request):
-    return render(request, "myapp/signup.html")
+    if request.method == 'POST':
+        form = forms.SignUpForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            image = form.cleaned_data.get('img')
+            user = authenticate(username=username, password=raw_password, img = image)
+            login(request, user)
+            return redirect('index')
+        else:
+            return render(request, "myapp/signup.html", {'form': form})
+    else:
+        form = forms.SignUpForm()
+    return render(request, 'myapp/signup.html', {'form': form})
 
-def login_view(request):
-    return render(request, "myapp/login.html")
+class MyLogin(LoginView):
+    form_class = forms.LoginForm
+    template_name = "myapp/login.html" 
+
 
 def friends(request):
-    return render(request, "myapp/friends.html")
+    friends = MyUser.objects.all()
+    context = {
+        'friends': friends
+    }
+    return render(request, "myapp/friends.html", context)
 
-def talk_room(request):
-    return render(request, "myapp/talk_room.html")
+def talk_room(request, username):
+    user = request.user
+    friend = MyUser.objects.get(username=username)
+    if request.method == "POST":
+        print(request.POST.get("content"))
+        ChatContent.objects.create(
+            send_to=friend,
+            send_from=user,
+            chat_content = request.POST.get("content")
+        )
+    chat_contents = ChatContent.objects.filter(
+        Q(send_to=user, send_from=friend) |
+        Q(send_to=friend, send_from=user)
+    )
+    context = {
+        "friend": friend,
+        "chat_contents": chat_contents
+    }
+    return render(request, "myapp/talk_room.html", context)
 
 def setting(request):
     return render(request, "myapp/setting.html")
