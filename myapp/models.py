a9285fee6f0fd418905fd55dc9b873613be969fd
Hatimitukunsan <marusu200391@gmail.com>
2023-02-01 19:05:44 +0900

    3 times first commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# superuser name : marusu pass : gameisgood
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    icon_image = models.ImageField(upload_to='media_local')
--- myapp/views.py
+++ myapp/views.py
@@ -18,3 +18,50 @@ def talk_room(request):
 
 def setting(request):
     return render(request, "myapp/setting.html")
+from django.shortcuts import redirect, render
+from .models import CustomUser
+from . import forms
+from .forms import SingupForm
+
+
+def index(request):
+    return render(request, "myapp/index.html")
+
+def signup_view(request):
+    form = forms.SingupForm()
+    return render(request, "myapp/signup.html", {'form': form})
+
+def login_view(request):
+    return render(request, "myapp/login.html")
+
+def friends(request):
+    return render(request, "myapp/friends.html")
+
+def talk_room(request):
+    return render(request, "myapp/talk_room.html")
+
+def setting(request):
+    return render(request, "myapp/setting.html")
+
+def register(request):
+    if request.method == 'POST':
+        form = SingupForm(request.POST,request.FILES)
+        if form.is_valid():
+            print('ok')
+            # ユーザー登録
+            CustomUser.objects.create(
+                username=request.POST['username'], 
+                email=request.POST['email'], 
+                password=request.POST['password'], 
+                icon_image=request.FILES['image']
+                )
+            return redirect('index')
+        else:
+            print('is not ok')
+            answer = form
+            form = SingupForm()
+            context = {
+                'form' : form,
+                'answer' : answer
+            }
+            return render(request, "myapp/signup.html", context)
