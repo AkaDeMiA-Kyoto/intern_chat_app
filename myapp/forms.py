da227a36f9240cf05d32334f0edfb114cd3692a1
Hatimitukunsan <marusu200391@gmail.com>
2023-02-01 09:42:22 +0900

    signup終了(nameとpassword酷似時のバリデーションは上手くできていないが…

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,1 +1,58 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
 
+class SingupForm(forms.Form):
+    username = forms.CharField(
+        label='Username',
+        required=True,
+        )
+
+    email = forms.EmailField(
+        label='Email Address',
+        required=True
+    )
+
+    password = forms.CharField(
+        label='password',
+        widget=forms.PasswordInput(),
+        min_length=8,
+        required=True
+    )
+
+    password_confirmation = forms.CharField(
+        label='password-confirmation',
+        widget=forms.PasswordInput(),
+        min_length=8,
+        required=True
+    )
+
+    image = forms.ImageField(
+        label='image',
+        required=False
+    )
+
+    def clean(self):
+        cleaced_data = super().clean()
+        password = cleaced_data.get('password')
+        password_confirmation = cleaced_data.get('password_confirmation')
+        if password != password_confirmation:
+            print('Error code:not right password')
+            raise forms.ValidationError(_('password-confirmation is not same as password'), code='not right password')
+
+
+        
+
+    # def check_password(self):
+    #     if self.cleaned_data['password_confirmation'] != self.cleaned_data['password']:
+    #         raise ValidationError('password different')
+
+    # バリデーション
+    # メールの形式ではない文字がemailフィールドに入力された時 →　完了
+    # パスワードと確認に差異が見られた時 →　完了
+    # ユーザーネームとパスワードが酷似していた時 → できていない
+    # パスワードが8文字未満の時　→　完了
+
+    # 参考
+    # https://itc.tokyo/django/form-validation/
+            
--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import redirect, render
 from .models import CustomUser
+from . import forms
+from .forms import SingupForm
 
 
 def index(request):
     return render(request, "myapp/index.html")
 
 def signup_view(request):
-    return render(request, "myapp/signup.html")
+    form = forms.SingupForm()
+    return render(request, "myapp/signup.html", {'form': form})
 
 def login_view(request):
     return render(request, "myapp/login.html")
@@ -21,5 +24,24 @@ def setting(request):
     return render(request, "myapp/setting.html")
 
 def register(request):
-    CustomUser.objects.create(username=request.POST['username'], email=request.POST['email'], password=request.POST['password'], icon_image=request.FILES['image'])
-    return redirect('index')
+    if request.method == 'POST':
+        form = SingupForm(request.POST,request.FILES)
+        if form.is_valid():
+            print('ok')
+            # ユーザー登録
+            CustomUser.objects.create(
+                username=request.POST['username'], 
+                email=request.POST['email'], 
+                password=request.POST['password'], 
+                icon_image=request.FILES['image']
+                )
+            return redirect('index')
+        else:
+            print('is not ok')
+            answer = form
+            form = SingupForm()
+            context = {
+                'form' : form,
+                'answer' : answer
+            }
+            return render(request, "myapp/signup.html", context)
