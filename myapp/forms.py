d759ae261c3289da71c6b7abcdff7cde621edc8b
koyashimano <koya_shimano0721@icloud.com>
2023-05-29 10:09:41 +0900

    signup page

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,1 +1,31 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
 
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(
+        label="Username",
+        max_length=40,
+        required=True
+    )
+    email = forms.EmailField(
+        label="Email",
+        required=True
+    )
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(),
+        min_length=8
+    )
+    password2 = forms.CharField(
+        label="Password(again)",
+        widget=forms.PasswordInput(),
+        min_length=8
+    )
+    image = forms.ImageField(
+        label="Image",
+    )
+    
+    class Meta:
+        model = CustomUser
+        fields = ("username", "email", "password1", "password2", "image")
