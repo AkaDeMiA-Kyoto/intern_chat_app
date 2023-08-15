61ac085b48eca2f6a115061c0caf6e4c79f0abee
eggkid <yukiyasudayy@gmail.com>
2023-08-15 19:37:23 +0900

    signup finished

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,1 +1,12 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.db import models
+from .models import CustomUser
 
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    image = forms.ImageField(required=True)
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'password1', 'password2', 'image')
--- myapp/views.py
+++ myapp/views.py
@@ -1,15 +1,33 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import CreateView
+from django.contrib.auth import login, authenticate
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse_lazy
+from .forms import SignUpForm
+from django.contrib.auth.forms import UserCreationForm
 
 
 def index(request):
     return render(request, "myapp/index.html")
 
 def signup_view(request):
-    return render(request, "myapp/signup.html")
+    if request.method == 'POST':
+        form = SignUpForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            if 'image' in request.FILES:
+                user.image = request.FILES['image']
+                user.save()
+            return redirect('index')
+    else:
+        form = SignUpForm(request.POST, request.FILES)
+    return render(request, "myapp/signup.html", {'form': form})
 
 def login_view(request):
     return render(request, "myapp/login.html")
 
+@login_required
 def friends(request):
     return render(request, "myapp/friends.html")
 
