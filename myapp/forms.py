7d44047764c5431820b5abb852fd79ea1f753148
[kuzekazu] <[kuzekazu.ck@gmail.com]>
2023-09-11 09:27:10 +0900

    commit message

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,1 +1,23 @@
 
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+from .models import User
+from django.contrib.auth.forms import AuthenticationForm
+"""
+def SignUpForm()
+"""
+# User = get_user_model()
+
+class SignupForm(UserCreationForm):
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', 'icon')
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = field.label
--- myapp/views.py
+++ myapp/views.py
@@ -1,16 +1,44 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
+from .forms import SignupForm, LoginForm
+from django.contrib.auth import authenticate, login, get_user_model
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
+User = get_user_model()
 
 def index(request):
     return render(request, "myapp/index.html")
 
 def signup_view(request):
-    return render(request, "myapp/signup.html")
+    if request.method == "GET":
+        form = SignupForm()
+        error_message = ''
+    elif request.method == "POST":
+        form = SignupForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+            return redirect("/")
+        else:
+            print(form.errors)
+    context = {
+        "form": form,
+    }
+    return render(request, "myapp/signup.html", context)
 
-def login_view(request):
-    return render(request, "myapp/login.html")
+class Login(LoginView):
+    authentication_form = LoginForm
+    template_name = "myapp/login.html"
+
+class Logout(LoginRequiredMixin, LogoutView):
+    template_name = "myapp/login.html"
 
 def friends(request):
+    
     return render(request, "myapp/friends.html")
 
 def talk_room(request):
