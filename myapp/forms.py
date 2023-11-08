2891a87556d5b376d537c0b9c9188526ad320d9a
matsumoto-kouta <matsumoto.kouta.for.business@gmail.com>
2023-11-08 13:13:52 +0900

    SignUp 完成

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,1 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
 
+from .models import CustomUser
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = (
+            "username",
+             "email",
+             "image",
+             )
--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,29 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect, render
-
+from .forms import CreateUserForm
 
 def index(request):
     return render(request, "myapp/index.html")
 
 def signup_view(request):
-    return render(request, "myapp/signup.html")
+    if request.method == 'POSt':
+        form = CreateUserForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username, password)
+            if user is not None:
+                login(request, user)
+            return redirect("/")
+        else:
+            print(form.errors)
+    else:
+        form = CreateUserForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "myapp/signup.html", context)
 
 def login_view(request):
     return render(request, "myapp/login.html")
