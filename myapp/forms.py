8e840eaba1e5b6b86cdf259c55a2d147cc45f727
matsumoto-kouta <matsumoto.kouta.for.business@gmail.com>
2023-11-08 17:05:38 +0900

    signup 完成

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,12 +1,17 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
 
 from .models import CustomUser
 
-class CreateUserForm(UserCreationForm):
+CustomUser = get_user_model()
+
+class SignUpForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = (
             "username",
              "email",
              "image",
+             "password1",
+             "password2",
              )
--- myapp/views.py
+++ myapp/views.py
@@ -1,28 +1,26 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect, render
-from .forms import CreateUserForm
+from .forms import SignUpForm
 
 def index(request):
     return render(request, "myapp/index.html")
 
 def signup_view(request):
-    if request.method == 'POSt':
-        form = CreateUserForm(request.POST, request.FILES)
+    if request.method == "POST":
+        form = SignUpForm(request.POST, request.FILES)
+        print("check3")
         if form.is_valid():
+            print("check1")
             form.save()
-            username = form.cleaned_data.get("username")
-            password = form.cleaned_data.get("password")
-            user = authenticate(username, password)
-            if user is not None:
-                login(request, user)
-            return redirect("/")
+            return redirect("index")
         else:
             print(form.errors)
     else:
-        form = CreateUserForm()
+            form = SignUpForm()
     context = {
         "form": form,
     }
+    print("check2")
     return render(request, "myapp/signup.html", context)
 
 def login_view(request):
