759d91aa9737b7b8b8a08cf59d8f9fae2fadb9b0
ichikawa <ichikawa.genji.34s@st.kyoto-u.ac.jp>
2024-08-10 20:56:22 +0900

    first commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
     return render(request, "myapp/index.html")
 
+
 def signup_view(request):
     return render(request, "myapp/signup.html")
+
 
 def login_view(request):
     return render(request, "myapp/login.html")
 
+
+@login_required
 def friends(request):
     return render(request, "myapp/friends.html")
 
+
 def talk_room(request):
     return render(request, "myapp/talk_room.html")
+
 
 def setting(request):
     return render(request, "myapp/setting.html")
