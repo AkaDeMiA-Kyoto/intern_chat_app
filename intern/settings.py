785248510cf48c1b9fed0c0d5f3498ce2975f55b
Daichi <daichi.bigshine777@gmail.com>
2024-12-24 23:37:22 +0900

    modefy setting

--- intern/settings.py
+++ intern/settings.py
@@ -123,9 +123,6 @@ USE_TZ = True
 import os
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "myapp", "static"),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
