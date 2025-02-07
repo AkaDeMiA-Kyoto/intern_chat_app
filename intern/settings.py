ae273ddba6b50b3a4edebf47f795e2ddcf32fcb2
Daichi <daichi.bigshine777@gmail.com>
2025-02-07 22:31:42 +0900

    本番環境のsettingsを分離

--- intern/settings.py
+++ intern/settings.py
@@ -1,1 +1,34 @@
-from .settings_dev import *
+from .settings_common import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS=[]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'Error',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'dev',
+        },
+    },
+    'formatters': {
+        'dev': {
+            'format': '\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(lineno)d)',
+                '%(message)s'
+            ]),
+        },
+    },
+}
