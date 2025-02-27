0494d0a318f19740b4de6d7debf07d2031d27883
ryryozaemon <8thakademia.nir@gmail.com>
2025-02-27 19:18:32 +0900

    テスト

--- myapp/urls.py
+++ myapp/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('friends/', views.getFriendsList, name='friends'),
     path('talk_room/', views.talk_room, name='talk_room'),
     path('setting/', views.setting, name='setting'),
+    path('password_change/', views.PasswordChange.as_view(), name='password_change'),
+    path('password_change/done/', views.PasswordChangeDone.as_view(), name='password_change_done'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
