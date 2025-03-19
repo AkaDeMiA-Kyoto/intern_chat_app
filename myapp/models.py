62b191a4ec247145cc267357b6759bed4d7f6bb5
hiiragi614 <shutaka1801@gmail.com>
2025-03-19 13:38:35 +0900

    finish+

--- myapp/models.py
+++ myapp/models.py
@@ -4,11 +4,11 @@ import uuid
 from django.utils.timezone import now
 
 class CustomUser(AbstractUser):
-    username = models.CharField(verbose_name='Username', max_length=15, null=False, blank=False, unique=True)
-    email = models.EmailField(verbose_name='Email', max_length=50, null=False, blank=False)
-    password1 = models.CharField(verbose_name='Password', max_length=30, null=False, blank=False, default="")
-    password2 = models.CharField(verbose_name='Password confirmation', max_length=30, null=False, blank=False, default="")
-    image = models.ImageField(verbose_name='Img', upload_to="", blank=False, null=False, default="")
+    username = models.CharField('Username', max_length=15, null=False, blank=False, unique=True)
+    email = models.EmailField('Email', max_length=50, null=False, blank=False)
+    # password1 = models.CharField('Password', max_length=30, null=False, blank=False, default="")
+    # password2 = models.CharField('Password confirmation', max_length=30, null=False, blank=False, default="")
+    image = models.ImageField('プロフィール画像', upload_to="", blank=False, null=False, default="")
     
     def __str__(self):
         return self.username
