345f01b4b499e231925fe21e8046a158487cd0ca
ka-ryo-ta <ryota.kaggie@gmail.com>
2025-04-17 00:19:20 +0900

    setting.pyにpostgresqlの設定

--- intern/settings.py
+++ intern/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'intern.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Users',
+        'USER': 'katyota',
+        'PASSWORD':'tfjkwapg',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
