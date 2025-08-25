4b91055edab6cc119cef9f9fa72ed178568f8a9e
tauqma <ishitaku0518@outlook.jp>
2025-08-25 15:32:00 +0900

    login

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,1 +1,18 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+from django.contrib.auth.forms import AuthenticationForm 
 
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    icon = forms.ImageField(required=False)
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'password1', 'password2', 'icon')
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control', 'placeholder': 'ユーザー名'})
+        self.fields['password'].widget.attrs.update({'class': 'form-control', 'placeholder': 'パスワード'})
--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import redirect, render
-
+from .forms import SignUpForm
+from django.contrib.auth.views import LoginView
+from .forms import LoginForm
 
 def index(request):
     return render(request, "myapp/index.html")
 
 def signup_view(request):
-    return render(request, "myapp/signup.html")
+    if request.method == 'POST':
+        form = SignUpForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('myapp:index')
+    else:
+        form = SignUpForm()
+    return render(request, 'myapp/signup.html', {'form':form})
 
-def login_view(request):
-    return render(request, "myapp/login.html")
+class LoginView(LoginView):
+    template_name = 'myapp/login.html'
+    authentication_form = LoginForm
 
 def friends(request):
     return render(request, "myapp/friends.html")
