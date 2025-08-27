a77c9eab99ce6968aa8b6d94e7cac46023d59633
yuka524 <suzaku33550336@gmail.com>
2025-08-27 16:14:31 +0900

    first commit

---
+++ myapp/settings.py
@@ -0,0 +1,5 @@
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media_local'
+
+LOGIN_URL = 'intern_chat_app:login_view'
+LOGIN_REDIRECT_URL = 'intern_chat_app:friends'
--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,49 @@
 from django.shortcuts import redirect, render
+from .forms import SignupForm, LoginForm
+from django.contrib.auth import login
 
 
 def index(request):
     return render(request, "myapp/index.html")
 
 def signup_view(request):
-    return render(request, "myapp/signup.html")
+    if request.method == 'POST':
+
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return render(request, 'myapp/index.html')
+
+
+    else:
+        form = SignupForm()
+    
+    param = {
+        'form': form
+    }
+
+    return render(request, 'myapp/signup.html', param)
 
 def login_view(request):
-    return render(request, "myapp/login.html")
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+
+            if user:
+                login(request, user)
+                return render(request, 'myapp/friends.html')
+
+    else:
+        form = LoginForm()
+
+    param = {
+        'form': form,
+    }
+
+    return render(request, "myapp/login.html", param)
 
 def friends(request):
     return render(request, "myapp/friends.html")
