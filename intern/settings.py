a36d435009953487a80831bd777a961e2641ce74
issei827 <maeda.issei.62e@st.kyoto-u.ac.jp>
2025-08-28 20:26:03 +0900

    commit

--- intern/settings.py
+++ intern/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os
+import environ
 
 from pathlib import Path
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ozyl(r!*=wht$a7^pp+wp=zg5g96yg5wz!7fwe$gq63874z9##'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -129,3 +131,12 @@ AUTH_USER_MODEL = 'myapp.CustomUser'
 LOGIN_REDIRECT_URL = "friends"
 TIME_ZONE='Asia/Tokyo'
 LOGOUT_REDIRECT_URL = "index"
+
+STATIC_ROOT = BASE_DIR/"staticfiles"
+
+if os.path.isfile('.env'):
+    env = environ.Env(DEBUG=(bool,False))
+    environ.Env.read_env('.env')
+
+    DEBUG = env('DEBUG')
+    ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
