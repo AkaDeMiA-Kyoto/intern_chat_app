12c6c26d072b7a02176f1694ded4a67a4ccc0589
sshi4307 <sshi4307@gmail.com>
2025-09-01 16:55:34 +0900

    settingの大枠まで

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,4 +1,30 @@
+# admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 from .models import CustomUser
 
-admin.site.register(CustomUser)
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    # 既存ユーザー編集画面
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+        ("プロフィール", {"fields": ("image",)}),  # ← 追加フィールド
+    )
+
+    # 新規作成画面
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "image", "password1", "password2"),
+        }),
+    )
+
+    list_display = ("username", "email", "is_staff", "is_active")
+    search_fields = ("username", "email")
+    ordering = ("id",)
