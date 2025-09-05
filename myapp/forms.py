f7cee416308bd0a95b2b4958cc4fdc91519a3061
rettu25252 <ishirettuu@gmail.com>
2025-09-05 22:31:31 +0900

    sign upの途中

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,1 +1,6 @@
+from django.contrib.auth.forms import UserCreationForm
 
+class SignUpForm(UserCreationForm):
+    # ここに、追加したいフィールドやバリデーションルールを記述できます。
+    # 例: ユーザー名やパスワードのバリデーションをカスタマイズする場合
+    pass
