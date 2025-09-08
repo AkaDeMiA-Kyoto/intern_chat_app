e64904f055a84294e68008ae5bd7f134f6e2b693
sshi4307 <sshi4307@gmail.com>
2025-09-08 19:44:49 +0900

    start

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
+from datetime import timedelta
+import secrets
 
 
 class User(AbstractUser):
@@ -8,7 +12,7 @@ class User(AbstractUser):
     )
 
 
-# トーク内容を全てdatbaseに保存する形をとる
+# トーク内容を全てdatabaseに保存する形をとる
 # ＞１個のトーク内容に紐づける情報は
 # ＞〇誰が送ったのか
 # ＞〇誰に送ったのか
@@ -28,3 +32,23 @@ class Talk(models.Model):
 
     def __str__(self):
         return "{}>>{}".format(self.talk_from, self.talk_to)
+    
+class EmailOTP(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="email_otps")
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField()
+    is_used = models.BooleanField(default=False)
+    attempts = models.PositiveSmallIntegerField(default=0)
+
+    @classmethod
+    def create_for(cls, user, ttl_minutes=10):
+        code = f"{secrets.randbelow(1_000_000):06d}"
+        return cls.objects.create(
+            user=user,
+            code=code,
+            expires_at=timezone.now() + timedelta(minutes=ttl_minutes),
+        )
+
+    def is_valid_now(self):
+        return (not self.is_used) and (self.expires_at >= timezone.now()) and (self.attempts < 5)
