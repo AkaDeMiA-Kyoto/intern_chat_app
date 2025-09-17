568a137c735e2253106d8e0a27f94eeb14bead04
rettu25252 <ishirettuu@gmail.com>
2025-09-17 20:13:26 +0900

    デプロイの何か

--- intern/settings.py
+++ intern/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os # 追加
+import environ # 追加
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ozyl(r!*=wht$a7^pp+wp=zg5g96yg5wz!7fwe$gq63874z9##'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -135,3 +137,13 @@ LANGUAGE_CODE = 'ja' # 使用言語を日本語にする
 TIME_ZONE = 'Asia/Tokyo' # タイムゾーンを東京にする
 
 LOGOUT_REDIRECT_URL = 'index'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# デプロイ環境のための設定(追加)
+if os.path.isfile('.env'): # .envファイルが存在しない時にもエラーが発生しないようにする
+    env = environ.Env(DEBUG=(bool, False))
+    environ.Env.read_env('.env')
+
+    DEBUG = env('DEBUG')
+    ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
