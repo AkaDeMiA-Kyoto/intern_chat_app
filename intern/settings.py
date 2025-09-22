0eac087444fb27d341db0abf7ab90cc432a4d3c7
tauqma <ishitaku0518@outlook.jp>
2025-09-22 20:25:58 +0900

    tauqma_deploy

--- intern/settings.py
+++ intern/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ozyl(r!*=wht$a7^pp+wp=zg5g96yg5wz!7fwe$gq63874z9##'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -131,3 +133,5 @@ AUTH_USER_MODEL = "myapp.CustomUser"
 
 LOGIN_REDIRECT_URL = '/friends'
 LOGOUT_REDIRECT_URL = '/logout_view'
+
+STATIC_ROOT = BASE_DIR/"staticfiles"
