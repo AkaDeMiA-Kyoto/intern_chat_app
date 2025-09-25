c180e186bda4529f7fc27af4b0af2039a6b7cd22
yutaka2 <yu0118777@gmail.com>
2025-09-25 20:25:59 +0900

    課題3修正

--- intern/settings.py
+++ intern/settings.py
@@ -158,7 +158,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = '/friends'
-LOGOUT_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = "index"
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # DEBUG_TOOLBAR_PANELS = [
