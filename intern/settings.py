c2a7cd7d12c57b916a5ad0e6e0cfc5575cc9d402
ivyak <bulletfall47@gmail.com>
2025-09-29 21:00:15 +0900

    Revert "complete2"
    This reverts commit 31f61297128e5d1e1889aa1da346d513f4f77987.

--- intern/settings.py
+++ intern/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'intern.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'myapp',
-        'USER': 'ivyak',
-        'PASSWORD': '7415Ring',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
