dfa6125dc5a0feff44a5d4bb09d90e22ecc8d2ad
ivyak <bulletfall47@gmail.com>
2025-09-29 21:00:08 +0900

    Revert "課題３前まで"
    This reverts commit 86ba3aebfe7c3e5ed117a40082e799aef718a439.

--- intern/settings.py
+++ intern/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'myapp',
         'USER': 'ivyak',
-        'PASSWORD': '',
+        'PASSWORD': '7415Ring',
         'HOST': 'localhost',
         'PORT': '5432',
     }
@@ -128,7 +128,6 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-# STATICFILES_DIRS = ["/home/ivyak/django_project/intern_chat_app/static"]
 
 
 # Default primary key field type
